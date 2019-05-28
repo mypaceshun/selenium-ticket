116fc65334116b7a526c20cc706a24869acce92d
Shun Kawai <shun@osstech.co.jp>
2019-05-28 12:15:32 +0900

    developにflake8を追加

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-
+import os
 import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+BASE_URL = 'https://ticket.line.me/'
+
 
 def main():
-    print('main')
+    print(os.environ['USERNAME'])
     options = Options()
     driver = webdriver.Chrome(chrome_options=options)
-    driver.get('https://google.co.jp')
+    driver.get(BASE_URL)
     driver.quit()
 
 
